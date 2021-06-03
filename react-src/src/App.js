a006d20e5f7fc8688cdf078f73c9ba04d9493656
MalteHusung <malte@curvaturegames.com>
2021-06-03 23:51:44 +0200

    added react app

---
+++ react-src/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from '@material-ui/core';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Button variant="outlined" color="primary">Test Button</Button>
+    </div>
+  );
+}
+
+export default App;
